8837992b9601aba162b3db4ae28f8377e0abf5f9
lukenvn <hcmcnova@gmail.com>
2019-02-14 22:52:24 +0700

    refactor code

--- src/main/java/dev/luke/CountingValeys.java
+++ src/main/java/dev/luke/CountingValeys.java
@@ -8,7 +8,7 @@ public class CountingValeys {
         int n = 8;
 
         int countingValleys = countingValleys(n, s);
-        System.out.println("number of count: "+countingValleys);
+        System.out.println("number of count: " + countingValleys);
 
     }
 
@@ -18,19 +18,19 @@ public class CountingValeys {
         int valleyCount = 0;
         int sum = 0;
         boolean mountain;
-        for (int i =0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
             char c = chars[i];
-            if (sum>0){
+            if (sum > 0) {
                 mountain = true;
-            }else {
-                mountain =false;
+            } else {
+                mountain = false;
             }
             if (c == 'U') {
                 sum += 1;
             } else if (c == 'D') {
                 sum -= 1;
             }
-            if(sum ==0 &&!mountain){
+            if (sum == 0 && !mountain) {
                 valleyCount++;
             }
 
