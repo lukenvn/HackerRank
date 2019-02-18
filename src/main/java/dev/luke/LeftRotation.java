e1c96c3626217cc7685397266d1fe8ea09da2a20
lukenvn <hcmcnova@gmail.com>
2019-02-18 22:51:33 +0700

    try

--- src/main/java/dev/luke/LeftRotation.java
+++ src/main/java/dev/luke/LeftRotation.java
@@ -7,7 +7,7 @@ public class LeftRotation {
 
     static int[] rotLeft(int[] a, int d) {
 
-        for (int i = 0; i <d; i++) {
+        for (int i = 0; i < d; i++) {
             rotateLeft(a);
         }
 
@@ -16,12 +16,12 @@ public class LeftRotation {
 
     private static void rotateLeft(int[] a) {
         int length = a.length;
-        int last = a[length-1];
-        a[length-1] = a[0];
-        for (int i =0; i < a.length-2; i++) {
-            a[i] = a[i+1];
+        int last = a[length - 1];
+        a[length - 1] = a[0];
+        for (int i = 0; i < a.length - 2; i++) {
+            a[i] = a[i + 1];
         }
-        a[length-2] = last;
+        a[length - 2] = last;
     }
 
 
